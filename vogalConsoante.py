64d9405735409caf873ee762b37c71e195ed82e2
Dandara <122398596+dandadivclass@users.noreply.github.com>
2023-06-06 22:30:32 -0300

    Update vogalConsoante.py

--- vogalConsoante.py
+++ vogalConsoante.py
@@ -3,15 +3,15 @@
 letra = input("Escolha uma letra:")
 vogal = ("A", "E", "I", "O", "U")
 
-if letra == "A":
+if letra.upper() == "A":
     print("A letra escolhida é uma vogal!")
-elif letra == "E":
+elif letra.upper() == "E":
     print("A letra escolhida é uma vogal!")
-elif letra == "I":
+elif letra.upper() == "I":
     print("A letra escolhida é uma vogal!")
-elif letra == "O":
+elif letra.upper() == "O":
     print("A letra escolhida é uma vogal!")
-elif letra == "U":
+elif letra.upper() == "U":
     print("A letra escolhida é uma vogal!")
 else:
     print("A letra escolhida é uma consoante!")
